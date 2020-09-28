ab67d34bfdada7b3e90b2f13e88aad6b632b84f1
cleiton pessoa <cleitonpessoa53@hotmail.com>
2020-09-28 13:27:44 -0300

    signup route created

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,18 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/index");
+
+router.post("/signup", async (req, res) => {
+	const password_hash = bcrypt.hashSync(req.body.password, 10);
+
+	await User.create({
+		...req.body,
+		password: password_hash
+    })
+    .catch(err => res.status(400).send(err))
+	.then(user => res.status(201).send(user));
+});
+
 
 module.exports = router;
