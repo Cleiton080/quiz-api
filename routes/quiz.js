2ac1c6a49841630c70ad6ee7a1ba71aae69432b7
cleiton pessoa <cleitonpessoa53@hotmail.com>
2020-10-01 01:27:15 -0300

    fetch all the quizzes by the authenticated user

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,4 +1,17 @@
 const router = require("express").Router();
+const { Quiz, User } = require("../models/index");
 
+router.get("/", async (req, res) => {
+    const userId = req.auth.id;
+
+    await User.findOne({
+        where: { id: userId },
+        include: {
+            model: Quiz
+        }
+    }).then(quizzes => {
+        res.status(200).json(quizzes);
+    });
+});
 
 module.exports = router;
