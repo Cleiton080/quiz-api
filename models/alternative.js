b84723cba5ca5b90e11a18fb1f3f61b7324aff99
cleiton pessoa <cleitonpessoa53@hotmail.com>
2020-10-06 02:07:07 -0300

    model alternative added

---
+++ models/alternative.js
@@ -0,0 +1,18 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Alternative extends Model {
+    static associate(models) {
+    }
+  };
+  Alternative.init({
+    alternative: DataTypes.TEXT('tiny'),
+    questionId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Alternative',
+  });
+  return Alternative;
+};
